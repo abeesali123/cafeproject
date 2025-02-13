6b84d29e7b69d9178ac2168d4629d4dde0a8576c
abees <abeesaliofc@gmail.com>
2025-02-14 02:25:48 +0530

    added

--- cafeproject/urls.py
+++ cafeproject/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
+from cafeproject import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 
